a6a43198b062e873a2be688e1545babf80b89601
MARTAKOZIOL <martaannakoziol@hotmail.com>
2020-08-05 16:10:56 +0100

    Format contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Form, Button } from 'semantic-ui-react';
+import './Contact.css';
 
 const GridExamplePadded = () => (
   <div style={{ fontSize: '1.5em', color: '#7d7d77', background: 'white' }}>
     <p style={{ fontSize: '2em' }}>Contact</p>
 
-    <Grid columns={1} padded style={{ lineHeight: '1.5em' }}>
-      <form method="post">
-        <input
-          style={{ width: '100%' }}
-          type="hidden"
-          name="form-name"
-          value="contact"
-        />
-
-        <input type="text" name="name" placeholder="Name" />
-
-        <input type="email" name="email" placeholder="Email" />
-
-        <textarea name="message" placeholder="Message" />
-
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+    <Grid padded>
+      <div style={{ textAlign: 'center', width: '100%', padding: '30px' }}>
+        <form method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <input type="text" name="name" placeholder="Name" />
+          <input type="email" name="email" placeholder="Email" />
+          <textarea name="message" placeholder="Message" />
+          <p>
+            <button style={{ margin: '20px' }} type="submit">
+              Send
+            </button>
+          </p>
+        </form>
+      </div>
     </Grid>
   </div>
 );
