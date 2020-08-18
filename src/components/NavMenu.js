3b8d83c90ff555535e0f694dec847c5c5bf2aab6
MARTAKOZIOL <martaannakoziol@hotmail.com>
2020-08-18 17:26:28 +0100

    Fix for position sticy on safari

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Menu, Sticky } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setActiveTab } from '../actions';
 import DropDown from './NavBarDropDown';
@@ -24,62 +24,65 @@ const NavMenu = ({ width }) => {
     };
   }, []);
   return (
-    <Sticky>
-      <Menu
-        fixed="top"
-        tabular
-        inverted
-        style={{ height: '77px', background: '#282c34', zIndex: '1000' }}
-        secondary
-      >
-        <Menu.Menu position="left" fixed="top">
+    <Menu
+      fixed="top"
+      tabular
+      inverted
+      style={{
+        position: '-webkit-sticky',
+        height: '77px',
+        background: '#282c34',
+        zIndex: '1000'
+      }}
+      secondary
+    >
+      <Menu.Menu position="left" fixed="top">
+        <Menu.Item
+          style={{
+            color: 'white',
+            fontSize: '30px'
+          }}
+          name="Marta Koziol"
+          active={activeItem === 'Marta Koziol'}
+          onClick={e =>
+            dispatch(setActiveTab('Marta Koziol')) && scrollToPosition(0)
+          }
+        />
+      </Menu.Menu>
+      {width > 900 ? (
+        <Menu.Menu position="right">
           <Menu.Item
-            style={{
-              color: 'white',
-              fontSize: '30px'
-            }}
-            name="Marta Koziol"
-            active={activeItem === 'Marta Koziol'}
-            onClick={e =>
-              dispatch(setActiveTab('Marta Koziol')) && scrollToPosition(0)
+            style={{ fontSize: '30px' }}
+            name="About Me"
+            active={activeItem === 'About Me'}
+            onClick={() =>
+              dispatch(setActiveTab('About Me')) &&
+              scrollToPosition(aboutMeOffsetTop - 77)
+            }
+          />
+          <Menu.Item
+            style={{ fontSize: '30px' }}
+            name="Interests"
+            active={activeItem === 'Interests'}
+            onClick={() =>
+              dispatch(setActiveTab('Interests')) &&
+              scrollToPosition(interestsOffsetTop - 77)
+            }
+          />
+          <Menu.Item
+            style={{ fontSize: '30px' }}
+            name="Contact"
+            active={activeItem === 'Contact'}
+            onClick={() =>
+              dispatch(setActiveTab('Contact')) &&
+              scrollToPosition(contactOffsetTop - 77)
             }
           />
         </Menu.Menu>
-        {width > 900 ? (
-          <Menu.Menu position="right">
-            <Menu.Item
-              style={{ fontSize: '30px' }}
-              name="About Me"
-              active={activeItem === 'About Me'}
-              onClick={() =>
-                dispatch(setActiveTab('About Me')) &&
-                scrollToPosition(aboutMeOffsetTop - 77)
-              }
-            />
-            <Menu.Item
-              style={{ fontSize: '30px' }}
-              name="Interests"
-              active={activeItem === 'Interests'}
-              onClick={() =>
-                dispatch(setActiveTab('Interests')) &&
-                scrollToPosition(interestsOffsetTop - 77)
-              }
-            />
-            <Menu.Item
-              style={{ fontSize: '30px' }}
-              name="Contact"
-              active={activeItem === 'Contact'}
-              onClick={() =>
-                dispatch(setActiveTab('Contact')) &&
-                scrollToPosition(contactOffsetTop - 77)
-              }
-            />
-          </Menu.Menu>
-        ) : (
-          <DropDown></DropDown>
-        )}
-      </Menu>
-    </Sticky>
+      ) : (
+        <DropDown></DropDown>
+      )}
+    </Menu>
   );
 };
 export default NavMenu;
